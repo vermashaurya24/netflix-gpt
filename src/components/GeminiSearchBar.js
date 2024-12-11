58775b2db6d05fe4f81bb468ba845d8b78078a54
vermashaurya24 <vermashaurya24@gmail.com>
2024-12-11 22:43:17 +0545

    feat: Gemini integrated.

--- src/components/GeminiSearchBar.js
+++ src/components/GeminiSearchBar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { lang } from '../utils/languageConstants';
 import { useSelector } from 'react-redux';
+import model from '../utils/gemini';
 
 const GeminiSearchBar = () => {
   const currentLanguage = useSelector(state => state.config?.language);
+  const searchText = useRef(null);
+  const handleGeminiSearch = async () => {
+    const result = await model.generateContent(searchText.current.value);
+    console.log(result.response.text());
+  }
+
   return (
     <div className='pt-[10%] flex justify-center z-10'>
-        <form className='w-1/2 bg-black bg-opacity-80 rounded-xl grid grid-cols-12'>
-            <input type='text' className='py-2 px-4 m-2 col-span-9 rounded-lg bg-black bg-opacity-50 border border-white' placeholder={lang[currentLanguage]?.geminiInputFieldText} />
-            <button className='py-2 px-4 m-2 bg-red-600 col-span-3 text-white rounded-lg'>{lang[currentLanguage]?.searchText}</button>
+        <form className='w-1/2 bg-black bg-opacity-80 rounded-xl grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
+            <input ref={searchText} type='text' className='py-2 px-4 m-2 col-span-9 rounded-lg bg-black bg-opacity-50 border text-white border-white' placeholder={lang[currentLanguage]?.geminiInputFieldText} />
+            <button className='py-2 px-4 m-2 bg-red-600 col-span-3 text-white rounded-lg' onClick={handleGeminiSearch}>{lang[currentLanguage]?.searchText}</button>
         </form>
     </div>
   );
--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -1,7 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-
-const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const REACT_APP_GEMINI_API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(REACT_APP_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default model;
