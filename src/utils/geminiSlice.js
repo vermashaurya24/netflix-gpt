83cd4bef5e4b6ab5b9a374f71e90aaa774e3b714
vermashaurya24 <vermashaurya24@gmail.com>
2024-12-19 23:02:16 +0545

    feat: Added a new state in redux to store movie search results.

--- src/utils/geminiSlice.js
+++ src/utils/geminiSlice.js
@@ -4,13 +4,17 @@ const geminiSlice = createSlice({
     name: "gemini",
     initialState: {
         showGeminiSearch: false,
+        geminiSearchMovieResults: []
     },
     reducers: {
         toggleGeminiSearch: (state) => {
             state.showGeminiSearch = !state.showGeminiSearch;
         },
+        addGeminiSearchMovieResults: (state, action) => {
+            state.geminiSearchMovieResults.push(action.payload);
+        }
     },
 });
 
 export default geminiSlice.reducer;
-export const {toggleGeminiSearch} = geminiSlice.actions;
+export const {toggleGeminiSearch, addGeminiSearchMovieResults} = geminiSlice.actions;
