39170b1b93a63738a589ab65f29a8f206fb582ac
vermashaurya24 <vermashaurya24@gmail.com>
2024-12-31 15:20:51 +0545

    feat: useBackgroundVideo hook now uses react query to fetch and process data.

--- src/hooks/useBackgroundVideo.js
+++ src/hooks/useBackgroundVideo.js
@@ -1,34 +1,40 @@
-import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSlice";
+import { useQuery } from "@tanstack/react-query";
 
 const useBackgroundVideo = (movieId) => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
 
   const getMovieVideos = async () => {
-    if (movieId) {
-      const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`;
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization: `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`,
-        },
-      };
-      const response = await fetch(url, options);
-      const videos = await response.json();
-      const filteredTrailers = videos?.results.filter(
-        (video) => video.type === "Trailer"
-      );
-      const trailerObject = filteredTrailers.length
-        ? filteredTrailers[0]
-        : videos?.results[0];
-      dispatch(addTrailerVideo(trailerObject));
-    }
+    const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`,
+      },
+    };
+    const response = await fetch(url, options);
+    return response.json();
   };
+
+  const { data } = useQuery({
+    queryKey: ["useBackgroundVideo", movieId],
+    queryFn: getMovieVideos,
+    enabled: !!movieId
+  });
+
+  const filteredTrailers = data?.results?.filter(
+    (video) => video.type === "Trailer"
+  );
+
+  const trailerObject = filteredTrailers?.length
+    ? filteredTrailers[0]
+    : data?.results[0];
+
+  if (trailerObject) {
+    dispatch(addTrailerVideo(trailerObject));
+  }
 };
 
 export default useBackgroundVideo;
