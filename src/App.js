cc1ac5efffb8a04704d692d3e2e5891d0bae0e87
vermashaurya24 <vermashaurya24@gmail.com>
2024-12-31 15:19:46 +0545

    feat: Configured our app to use react-query, rather than manually fetching the data inside a useEffect.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Provider store={appStore}>
-      <Body />
+      <QueryClientProvider client={queryClient}>
+        <Body />
+      </QueryClientProvider>
     </Provider>
   );
 }
