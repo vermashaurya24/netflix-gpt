ef15cba189372c566a765cdb5be210de9a33e13e
vermashaurya24 <vermashaurya24@gmail.com>
2025-01-08 21:52:27 +0545

    feat: Disabled the refetchOnWindowFocus key for queryClient. Reduces the number of API calls made.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+      queries: {
+          refetchOnWindowFocus: false,
+      },
+  },
+});
 
 function App() {
   return (
