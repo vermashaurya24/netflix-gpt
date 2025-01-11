433d2aa7ef67a0ea25861a5e2bdcb53f71bfa6b0
vermashaurya24 <vermashaurya24@gmail.com>
2025-01-11 21:49:21 +0545

    chore: Reduced the movie card and list title size for smaller screens.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,7 +6,7 @@ const MovieCard = ({movie}) => {
     return;
   }
   return (
-    <div className='w-48 pr-4'>
+    <div className='w-36 sm:w-48 pr-4'>
       <img className='rounded-lg' alt='Movie Poster' src={POSTER_URL+movie?.poster_path} />
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,8 +2,8 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ category, data }) => {
   return (
-    <div className="p-4 text-white ml-8">
-      <h1 className="text-3xl pt-2 pb-1">{category}</h1>
+    <div className="p-4 text-white ml-4 md:ml-8">
+      <h1 className="text-lg md:text-3xl pt-2 pb-1">{category}</h1>
       <div className="flex overflow-x-scroll">
         <div className="flex">
           {data?.map((movie) => (
